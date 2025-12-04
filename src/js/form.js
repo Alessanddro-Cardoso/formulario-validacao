a4a224c5c1df3d8a4925b9484621db95eb7c233a
Alessandro Cardoso <alecardoso004@gmail.com>
2025-12-04 17:52:54 -0300

    adicionado o arquivo formulario de validacao

---
+++ src/js/form.js
@@ -0,0 +1,23 @@
+// Validação adicional e manipulação do envio
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (pagina) => {
+  pagina.preventDefault();
+
+  const envio = formulario.querySelectorAll("input, textarea");
+
+  envio.forEach((campo) => {
+    const dado = campo.value.trim();
+    const campoObg = campo.parentElement.querySelector(".obrigatorio");
+
+    campo.classList.remove("camponulo", "preenchido");
+    campoObg.classList.remove("nulo");
+
+    if (dado === "") {
+      campo.classList.add("camponulo");
+      campoObg.classList.add("nulo");
+    } else {
+      campo.classList.add("preenchido");
+    }
+  });
+});
